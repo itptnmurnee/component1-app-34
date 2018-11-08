c65bd8cabf3abdc21b55cc892ae0c7fecf9a757c
murnee che-mae <cmurnee@ptnicec.ac.th>
2018-11-08 17:54:28 +0700

    send project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController, ActionSheetController} from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController,
+    public actionSheet: ActionSheetController,
+    public navCtrl: NavController) {
 
-  }
+    }
+
+    showAction(){
+      const acSheet = this.actionSheet.create({
+        title: 'Test Action',
+        buttons: [
+          {
+            text: 'Information',
+            role: 'Info',
+            handler: () => {
+              console.log('Info Clicked');
+            }
+          }
+        ]
+      });
+        acSheet.present();
+    }
+
+    showAlert1(){
+      const alert = this.alertCtrl.create({
+        title: 'Alert Data!!!',
+        subTitle: 'Musnee',
+        buttons: [
+          {
+            text: 'OK'
+          },
+          {
+            text: 'Cancel',
+            handler: () => {
+              console.log('Cancel Data');
+            }
+          }
+        ]
+      });
+      alert.present();
+    }
 
 }
